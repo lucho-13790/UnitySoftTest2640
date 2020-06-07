99e2907a8b644505c37b4074277978737fc97525
lucho-13790 <63757395+lucho-13790@users.noreply.github.com>
2020-06-07 10:34:41 -0500

    Add files via upload

--- clientesUnitysoft/src/main/java/co/com/javeriana/controller/ClientController.java
+++ clientesUnitysoft/src/main/java/co/com/javeriana/controller/ClientController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +28,7 @@ public class ClientController {
 	@Autowired
 	SaveClients clientServices;
 	
-	@PostMapping(value = "/{userName}")
+	@PostMapping(value = "/{userName}/client")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "guardo exitosamente",response= ResponseModel.class),
 	@ApiResponse(code = 412, message = "el usuario ya existe",response= ResponseModel.class),
 	@ApiResponse(code = 500, message = "error en el servicio",response= ResponseModel.class)})
@@ -37,5 +40,44 @@ public class ClientController {
 		return new ResponseEntity<>("Se guardo correctamente",HttpStatus.OK);
 	
 	}
-
+	@GetMapping(value = "/{userName}")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "consulta exitosa",response= Client.class),
+	@ApiResponse(code = 412, message = "el usuario ya existe",response= ResponseModel.class),
+	@ApiResponse(code = 500, message = "error en el servicio",response= ResponseModel.class)})
+	@ResponseBody
+	public ResponseEntity<Object> getUser(@PathVariable(name = "userName", required = true) String userName){
+						
+		return new ResponseEntity<>(clientServices.findbyId(userName),HttpStatus.OK);
+	
+	}
+	@GetMapping(value = "/allusers")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "consulta exitosa"),
+	@ApiResponse(code = 500, message = "error en el servicio",response= ResponseModel.class)})
+	@ResponseBody
+	public ResponseEntity<Object> getAllUser(){
+						
+		return new ResponseEntity<>(clientServices.findAll(),HttpStatus.OK);
+	
+	}
+	@PatchMapping(value = "update/{userName}")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "consulta exitosa"),
+	@ApiResponse(code = 500, message = "error en el servicio",response= ResponseModel.class)})
+	@ResponseBody
+	public ResponseEntity<Object> updateUser(@PathVariable(name = "userName", required = true) String userName,
+			@RequestBody Client input){
+		input.setUserName(userName);
+		clientServices.update(input);	
+		return new ResponseEntity<>("se acualizo correctamente",HttpStatus.OK);
+	
+	}
+	@DeleteMapping(value = "delete/{userName}")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "consulta exitosa"),
+	@ApiResponse(code = 500, message = "error en el servicio",response= ResponseModel.class)})
+	@ResponseBody
+	public ResponseEntity<Object> deleteUser(@PathVariable(name = "userName", required = true) String userName){
+		
+		clientServices.delete(userName);	
+		return new ResponseEntity<>("se borro correctamente",HttpStatus.OK);
+	
+	}
 }
